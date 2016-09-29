90e1dbec7885d2de80987980f5b65ff577b95298
Keith Sparkjoy <keith@sparkjoyfoundation.com>
2016-09-29 15:02:46 -0600

    Add end steps

--- src/pfdplayer/vm/pfd.cljs
+++ src/pfdplayer/vm/pfd.cljs
@@ -1,27 +1,31 @@
 (ns pfdplayer.vm.pfd)
 
+(defn- add-end
+  [steps]
+  (conj steps {:t :end}))
+
 (defn pfd->vm
   [pfd {:keys [begin-x begin-y step-width step-height nva-offset]} zoom]
-  {:viewport-height (+ begin-y (* step-height (count (:steps pfd))))
-   :viewport-width (+ begin-x
-                      (* step-width
-                         (inc (apply max
-                                     (map #(count (:nva %))
-                                          (:steps pfd))))))
-   :steps (map-indexed
-           (fn [n step]
-             (let [y (+ begin-y (* n step-height))]
-               (assoc step
-                      :x begin-x
-                      :y y
-                      :nva (map-indexed
-                            (fn [n step]
-                              (assoc step
-                                     :x (+ begin-x
-                                           nva-offset
-                                           (* (inc n) step-width))
-                                     :y y))
-                            (:nva step)))))
-           (:steps pfd))
-   :zoom zoom})
+  (let [steps (map-indexed
+               (fn [n step]
+                 (let [y (+ begin-y (* n step-height))]
+                   (assoc step
+                          :x begin-x
+                          :y y
+                          :nva (map-indexed
+                                (fn [n step]
+                                  (assoc step
+                                         :x (+ begin-x
+                                               nva-offset
+                                               (* (inc n) step-width))
+                                         :y y))
+                                (add-end (:nva step))))))
+               (add-end (:steps pfd)))]
+    {:viewport-width (+ begin-x
+                        (* step-width
+                           (inc (apply max
+                                       (map #(count (:nva %)) steps)))))
+     :viewport-height (+ begin-y (* step-height (count steps)))
+     :steps steps
+     :zoom zoom}))
 
